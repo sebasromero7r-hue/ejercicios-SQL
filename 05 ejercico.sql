c52ca148655fb0d37ca2c4d747015ad0abd87cb7
sebasromero7r-hue <sebasromero7r@gmail.com>
2026-02-26 21:44:06 -0500

    AGREGUE SOLUCION EJERCICIO 5

---
+++ 05 ejercico.sql
@@ -0,0 +1,5 @@
+-- Ejercicio 5 Muestra el nombre, correo y fecha de nacimiento de todos los clientes, ordenados 
+-- alfabéticamente por nombre. 
+SELECT nombre, correo, fecha_nacimiento
+FROM clientes
+ORDER BY nombre ASC;
